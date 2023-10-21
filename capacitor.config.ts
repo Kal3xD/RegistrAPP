8cbafc68dbce2550c5b6224a9da368ab15e49c07
bayronfu <bayr.fuentes@duocuc.cl>
2023-10-20 23:52:04 -0300

    ww

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'AppEntregable',
+  appId: 'com.registrap.app',
+  appName: 'registrap',
   webDir: 'www',
   server: {
     androidScheme: 'https'
